2ae5c6abc31a660084c5f18c76dfdb675f687c78
Ellis <nardi0302@gmail.com>
2023-04-07 16:10:31 +0900

    Config: Setup default settings for vite and storybook
    - 절대경로 사용을 위한 IDE, vite, storybook 설정
    - 접근성 및 상호작용 테스트를 위한 storybook addon 추가
    - storybook 배포 및 before/after 확인을 위한 chromatic 추가

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,10 +1,14 @@
+import { resolve } from "path";
 import type { StorybookConfig } from "@storybook/react-vite";
+import { mergeConfig } from "vite";
+
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "@storybook/addon-a11y",
   ],
   framework: {
     name: "@storybook/react-vite",
@@ -12,6 +16,15 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: "tag",
+  },
+  async viteFinal(config) {
+    return mergeConfig(config, {
+      resolve: {
+        alias: {
+          "@": resolve("./src"),
+        },
+      },
+    });
   },
 };
 export default config;
